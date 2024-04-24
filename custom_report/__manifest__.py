78a786e35eaa81dd42717d89a68afc95a56bc33e
Hasan <hasanchaudharyy@gmail.com>
2024-04-24 15:39:44 +0500

    eco-NG

---
+++ custom_report/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Report Customization',
+    'version': '17.0',
+
+    "author": "Rao Mubasher Hussain",
+    "website": """ """,
+    'company': '',
+    'maintainer': 'Rao Mubasher Hussain',
+
+    'depends': ['base', 'sale', 'account'],
+    'license': 'LGPL-3',
+    'category': 'Productivity',
+
+    'summary': """ Report Customization, Add background/cover image """,
+    'description': """ Report Customization, Add background/cover image """,
+
+    'data': [
+
+        'report/report_action.xml',
+        'report/report_invoice_temp.xml',
+        'report/report_saleorder_temp.xml',
+
+    ],
+
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+
+}
